42d9bee71f8fbd85c87b38a5da307df5a33dec4a
ragerber <ragerber@student.ethz.ch>
2026-05-12 23:03:30 +0200

    added useful things for cluster

---
+++ cluster_commands.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+CLUSTER_USER="ragerber"
+CLUSTER_HOST="student-cluster.inf.ethz.ch"
+CLUSTER_DIR="/home/ragerber"
+
+copytocluster() {
+	scp "$1" "$CLUSTER_USER@$CLUSTER_HOST:$CLUSTER_DIR/"
+}
+
+copyfromcluster() {
+	scp "$CLUSTER_USER@$CLUSTER_HOST:$CLUSTER_DIR/$1" .
+}
+
+sshintocluster() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST"
+}
+
+getjobid() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST" "squeue | grep '$CLUSTER_USER'"
+}
+
+trackjob() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST" "tail -f '$CLUSTER_DIR/logs/depth_fusion-$1.out'"
+}
+
+jobcpu() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST" "srun --jobid='$1' --pty nvidia-smi"
+}
+startjob() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST" "cd '$CLUSTER_DIR' && sbatch train.sbatch"
+}
+
+geterrors() {
+	ssh "$CLUSTER_USER@$CLUSTER_HOST" "cat '$CLUSTER_DIR/logs/depth_fusion-$1.err'"
+}
